53beb44e91e3873e377478cb3b7b0e7cc95f7a1f
Paul Brémond <87093237+pbremondFR@users.noreply.github.com>
2026-05-09 21:28:41 +0200

    feat: move texture loading to sequence save, instead of on popup open

--- src/load_sequences_from_dir.cpp
+++ src/load_sequences_from_dir.cpp
@@ -11,6 +11,59 @@
 
 namespace fs = std::filesystem;
 
+/// File names of images to load from disk to GPU once the list is saved.
+struct FigureFilenames
+{
+	std::string filename_form_b;
+	std::string filename_form_c;
+	int k_factor = 0;
+
+	// Loads textures to GPU VRAM from file names
+	[[nodiscard]] std::optional<Figure> load_textures_in_vram() const noexcept
+	{
+		// printf("%s: %s\n", __FUNCTION__, filename_form_b.c_str());
+		Figure new_figure = {
+			.texture_form_b = LoadTexture(filename_form_b.c_str()),
+			.texture_form_c = LoadTexture(filename_form_c.c_str()),
+			.k_factor = k_factor,
+		};
+		if (!new_figure.texture_form_b.IsValid() || !new_figure.texture_form_c.IsValid())
+			return std::nullopt;
+		return new_figure;
+	}
+};
+
+/// Temporary data about the sequence, which will be converted into full SequenceData when needed.
+/// Instead of storing a list of figures, it just stores their texture filenames, to be loaded in VRAM later.
+struct SequenceTemporaryData
+{
+	SequenceInfo info = {};
+	std::vector<FigureFilenames> figure_filenames = {};
+	size_t hash = 0;
+
+	/// Loads all figures in VRAM and returns newly created SequenceData object. If one or more file loads fails,
+	/// returns nullopt.
+	[[nodiscard]] std::optional<SequenceData> to_sequence_data() const
+	{
+		if (figure_filenames.empty())
+			return std::nullopt;
+
+		std::vector<Figure> transformed_figures;
+		for (FigureFilenames const& filenames : figure_filenames)
+		{
+			if (auto figure = filenames.load_textures_in_vram())
+				transformed_figures.push_back(std::move(*figure));
+			else
+				return std::nullopt; // Better to nullify the sequence entirely rather than have one texture missing
+		}
+		return SequenceData{
+			.info = info,
+			.figures = std::move(transformed_figures),
+			.hash = hash,
+		};
+	}
+};
+
 static std::optional<size_t> get_file_hash(fs::path const& dotseq_file_path)
 {
 	std::ifstream filestream(dotseq_file_path.string());
@@ -51,9 +104,9 @@ static fs::path get_exported_textures_path(size_t file_hash)
  * @param seq XML <sequence> node object, so we can lookup properties such as K-factors for each figure
  * @return Vector of figures, all loaded into the GPU. Empty if not found or unexpected error.
  */
-static std::vector<Figure> fetch_exported_textures_of_sequence(fs::path const& textures_path, pugi::xml_node const& seq)
+static std::vector<FigureFilenames> fetch_exported_textures_of_sequence(fs::path const& textures_path, pugi::xml_node const& seq)
 {
-	std::vector<Figure> sequences;
+	std::vector<FigureFilenames> sequences;
 
 	const fs::path form_b_path = textures_path / "Form_B";
 	const fs::path form_c_path = textures_path / "Form_C";
@@ -87,22 +140,22 @@ static std::vector<Figure> fetch_exported_textures_of_sequence(fs::path const& t
 	// Load textures for each texture of forms B and C
 	for (int i = 0; i < form_b_filenames.size(); i++)
 	{
-		Figure figure = {
-			.texture_form_b = LoadTexture(form_b_filenames[i].c_str()),
-			.texture_form_c = LoadTexture(form_c_filenames[i].c_str()),
+		FigureFilenames figure = {
+			.filename_form_b = form_b_filenames[i].c_str(),
+			.filename_form_c = form_c_filenames[i].c_str(),
 			.k_factor = k_factors[i],
 		};
 		// One texture couldn't be loaded into memory, don't want a corrupted image list so discard this program entirely...
-		if (figure.texture_form_b.id == 0 || figure.texture_form_c.id == 0)
+		if (figure.filename_form_b.empty() || figure.filename_form_c.empty())
 			return {};
 		sequences.push_back(std::move(figure));
 	}
 	return sequences;
 }
 
-static std::vector<SequenceData> get_sequences_from_path(fs::path const& path)
+static std::vector<SequenceTemporaryData> get_sequences_from_path(fs::path const& path)
 {
-	std::vector<SequenceData> sequences;
+	std::vector<SequenceTemporaryData> sequences;
 
 	if (!fs::is_directory(path))
 		return sequences;
@@ -121,8 +174,8 @@ static std::vector<SequenceData> get_sequences_from_path(fs::path const& path)
 		if (!result || doc.child("sequence").empty())
 			continue;
 		pugi::xml_node seq = doc.child("sequence");
-		std::vector<Figure> figures = fetch_exported_textures_of_sequence(texture_dir, seq);
-		sequences.emplace_back(SequenceData{
+		std::vector<FigureFilenames> figures = fetch_exported_textures_of_sequence(texture_dir, seq);
+		sequences.emplace_back(SequenceTemporaryData{
 			.info = SequenceInfo{
 				.file_name = reinterpret_cast<const char*>(entry.path().stem().u8string().c_str()),
 				.pilot_name = seq.child("pilot").text().as_string("???"),
@@ -133,7 +186,7 @@ static std::vector<SequenceData> get_sequences_from_path(fs::path const& path)
 				.sequence_text = seq.child("sequence_text").text().as_string(""),
 				.number_of_figures = static_cast<int>(figures.size()),
 			},
-			.figures = std::move(figures),
+			.figure_filenames = std::move(figures),
 			.hash = *file_hash,
 		});
 	}
@@ -195,7 +248,7 @@ void sequence_directory_modal()
 	if (ImGui::BeginPopupModal("Sequence list", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove))
 	{
 		static widget::DirectoryInputText sequences_path_widget;
-		static std::vector<SequenceData> sequences_in_dir;
+		static std::vector<SequenceTemporaryData> sequences_in_dir;
 
 		// Don't want the length of the path selector to be too short
 		ImGui::SetNextItemWidth(500);
@@ -223,12 +276,12 @@ void sequence_directory_modal()
             ImGui::PushItemFlag(ImGuiItemFlags_AllowDuplicateId, true);
 			for (int i = 0; i < sequences_in_dir.size(); ++i)
 			{
-				SequenceData const& sequence_data = sequences_in_dir[i];
+				SequenceTemporaryData const& sequence_data = sequences_in_dir[i];
 				SequenceInfo const& sequence_info = sequences_in_dir[i].info;
 
 				ImGui::TableNextRow();
 				ImGui::PushID(sequence_info.file_name.c_str());
-				ImGui::TableSetColumnIndex(0); bool do_load = sequence_load_button(sequences_in_dir[i].figures.size() > 0);
+				ImGui::TableSetColumnIndex(0); bool do_load = sequence_load_button(sequences_in_dir[i].figure_filenames.size() > 0);
 				if (do_load)
 				{
 					fs::path file_path = sequences_path_widget.get_path() / (sequence_info.file_name + ".seq");
@@ -253,11 +306,25 @@ void sequence_directory_modal()
 
 		if (ImGui::Button("Save"))
 		{
-			std::erase_if(sequences_in_dir, [](SequenceData const& seq) { return seq.figures.empty(); });
-			g_state.sequence_list = std::move(sequences_in_dir);
-			// Reset vector after std::move, just to be sure it's valid if it doesn't get re-computed (it should,
-			// because next time popup is open it will rescan files, but better safe than sorry)
-			sequences_in_dir.clear();
+			// Somebody explain to me how THAT is better than a simple loop like below :(
+			// Rust felt better at doing this man
+			// // Sequences that were *maybe* loaded into VRAM, need to check optional!
+			// auto maybe_loaded_sequences = sequences_in_dir
+			// 	| std::views::transform([](auto& seq) { return seq.to_sequence_data(); })
+			// 	| std::ranges::to<std::vector>();
+			// // Filtered failed sequences to only keep ones that were actually loaded
+			// auto new_sequence_list = maybe_loaded_sequences
+			// 	| std::views::filter([](auto const& opt_seq) { return opt_seq.has_value(); })
+			// 	| std::views::transform([](auto&& opt_seq) { return std::move(*opt_seq); })
+			// 	| std::ranges::to<std::vector>();
+
+			std::vector<SequenceData> new_sequence_list;
+			for (SequenceTemporaryData const& temp_data : sequences_in_dir)
+			{
+				if (auto full_data = temp_data.to_sequence_data())
+					new_sequence_list.push_back(std::move(*full_data));
+			}
+			g_state.sequence_list = std::move(new_sequence_list);
 			g_state.sequences_dir = sequences_path_widget.get_path();
 			ImGui::CloseCurrentPopup();
 		}
