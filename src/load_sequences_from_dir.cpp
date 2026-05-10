33ec59f5b1b1feeeab2326e37c9f7e4f8554c28d
Paul Brémond <87093237+pbremondFR@users.noreply.github.com>
2026-05-10 19:01:42 +0200

    feat: stop UI from hanging when loading images, have progress bar instead

--- src/load_sequences_from_dir.cpp
+++ src/load_sequences_from_dir.cpp
@@ -22,7 +22,7 @@ struct FigureFilenames
 	std::string filename_form_c;
 	int k_factor = 0;
 
-	// Loads textures to GPU VRAM from file names
+	/// Loads textures to GPU VRAM from file names. Will block the current thread, as such isn't very efficient.
 	[[nodiscard]] std::optional<Figure> load_textures_in_vram() const noexcept
 	{
 		// printf("%s: %s\n", __FUNCTION__, filename_form_b.c_str());
@@ -37,6 +37,28 @@ struct FigureFilenames
 	}
 };
 
+/// Intermediate representation of a figure by holding the form B and C *Images* (that is, image files loaded into
+/// RAM). This will end up being converted into Textures (use load_textures_in_vram())
+struct FigureImages
+{
+	raylib::Image image_form_b;
+	raylib::Image image_form_c;
+	int k_factor = 0;
+
+	[[nodiscard]] std::optional<Figure> load_textures_in_vram() const noexcept
+	{
+		Figure figure = {
+			.texture_form_b = LoadTextureFromImage(image_form_b),
+			.texture_form_c = LoadTextureFromImage(image_form_c),
+			.k_factor = k_factor,
+		};
+		if (!figure.texture_form_b.IsValid() || !figure.texture_form_c.IsValid())
+			return std::nullopt;
+		else
+			return figure;
+	}
+};
+
 /// Temporary data about the sequence, which will be converted into full SequenceData when needed.
 /// Instead of storing a list of figures, it just stores their texture filenames, to be loaded in VRAM later.
 struct SequenceTemporaryData
@@ -48,26 +70,26 @@ struct SequenceTemporaryData
 	/// Whether files from this sequence are already cached (found in the directory of get_exported_textures_path)
 	[[nodiscard]] bool are_files_cached() const noexcept { return !figure_filenames.empty(); }
 
-	/// Loads all figures in VRAM and returns newly created SequenceData object. If one or more file loads fails,
-	/// returns nullopt.
-	[[nodiscard]] std::optional<SequenceData> to_sequence_data() const
+	/// Loads all the figure images into RAM. Returns the corresponding vector, or an empty optional if one or more
+	/// images failed to load.
+	[[nodiscard]] auto load_sequence_images_to_RAM() const -> std::optional<std::vector<FigureImages>>
 	{
-		if (!are_files_cached())
-			return std::nullopt;
+		std::vector<FigureImages> result;
 
-		std::vector<Figure> transformed_figures;
 		for (FigureFilenames const& filenames : figure_filenames)
 		{
-			if (auto figure = filenames.load_textures_in_vram())
-				transformed_figures.push_back(std::move(*figure));
+			FigureImages images = {
+				.image_form_b = LoadImage(filenames.filename_form_b.c_str()),
+				.image_form_c = LoadImage(filenames.filename_form_c.c_str()),
+				.k_factor = filenames.k_factor,
+			};
+
+			if (images.image_form_b.data && images.image_form_c.data)
+				result.emplace_back(images);
 			else
-				return std::nullopt; // Better to nullify the sequence entirely rather than have one texture missing
+				return std::nullopt;
 		}
-		return SequenceData{
-			.info = info,
-			.figures = std::move(transformed_figures),
-			.hash = hash,
-		};
+		return result;
 	}
 };
 
@@ -263,6 +285,105 @@ static bool erase_successful_finished_threads()
 	return num_threads_finished > 0;
 }
 
+static void do_export_all_dotseq_files(
+	fs::path const& sequences_dir,
+	std::vector<SequenceTemporaryData> const& sequences_in_dir
+	)
+{
+	for (SequenceTemporaryData const& sequence : sequences_in_dir)
+	{
+		if (sequence.are_files_cached())
+			continue;
+		const fs::path file_path = sequences_dir / (sequence.info.file_name + ".seq");
+		const fs::path textures_path = get_exported_textures_path(sequence.hash);
+		if (auto thread = g_state.export_threads[sequence.hash].launch(file_path, textures_path))
+		{
+			thread->detach();
+		}
+	}
+}
+
+/**
+ * For all sequences in directory, loads all their image files into RAM. Stores the result into a map, associating
+ * sequence hash to images. The reason this doesn't straight up go to VRAM is because we need to put this in a thread,
+ * and we can't load into VRAM from somewhere else than the main thread.
+ *
+ * @param sequences_in_dir All sequences in the directory
+ * @param loaded_images Pointer to map associating sequence hash to sequence images
+ * @param loaded_progress Float representing load progress for 0 to 1.
+ */
+static void load_all_sequences_images_to_ram(
+	std::vector<SequenceTemporaryData> sequences_in_dir,
+	std::atomic<std::shared_ptr<std::unordered_map<size_t, std::vector<FigureImages>>>>& loaded_images,
+	std::atomic<float>& loaded_progress
+	)
+{
+	// Don't run thread if one is already running
+	if (loaded_images.load() != nullptr)
+		return;
+
+	// Working copy where we can wait all we want for image loading
+	auto working_map = std::make_shared<std::unordered_map<size_t, std::vector<FigureImages>>>();
+
+	loaded_progress = 0.0f;
+	const float delta = 1.0f / static_cast<float>(sequences_in_dir.size());
+	for (SequenceTemporaryData const& temp_data : sequences_in_dir)
+	{
+		if (auto images_in_ram = temp_data.load_sequence_images_to_RAM())
+			working_map->emplace(temp_data.hash, std::move(*images_in_ram));
+		loaded_progress += delta;
+	}
+	// Exchange pointers
+	loaded_images = working_map;
+}
+
+/**
+ * From current sequences that are in directory and previously loaded sequences images, creates the final
+ * vector of SequenceData by moving images into VRAM.
+ *
+ * @param sequences_in_dir All sequences in directory
+ * @param sequence_images Map associating sequence hash to previously loaded sequence images (loaded into RAM)
+ * @return Final vector of sequences to be used by the application
+ */
+static std::vector<SequenceData> load_all_sequence_textures_to_vram(
+	std::vector<SequenceTemporaryData> sequences_in_dir,
+	std::unordered_map<size_t, std::vector<FigureImages>> const& sequence_images
+	)
+{
+	// Transforms a vector of images to a vector of textures. Returns nullopt if one or more load fails.
+	auto load_textures_to_vram = [](std::vector<FigureImages> const& images) -> std::optional<std::vector<Figure>>
+	{
+		std::vector<Figure> result;
+		for (FigureImages const& image : images)
+		{
+			if (auto textures = image.load_textures_in_vram(); textures.has_value())
+				result.emplace_back(std::move(*textures));
+			else
+				return std::nullopt;
+		}
+		return result;
+	};
+	std::vector<SequenceData> new_sequences;
+
+	for (SequenceTemporaryData const& temp_data : sequences_in_dir)
+	{
+		auto images = sequence_images.find(temp_data.hash);
+		if (images == sequence_images.end())
+			continue; // Image never loaded in the RAM, skip the sequence
+
+		auto textures = load_textures_to_vram(images->second);
+		if (!textures.has_value())
+			continue; // Failed to move images to GPU VRAM
+
+		new_sequences.emplace_back(SequenceData{
+			.info = temp_data.info,
+			.figures = std::move(*textures),
+			.hash = temp_data.hash,
+		});
+	}
+	return new_sequences;
+}
+
 // TODO: Add a progress bar for loading (long time if script needs to be ran!!!). Other thread?
 // TODO: Differential move based on sequence hash (+ file name?) to only save sequences that are newly added
 //  (avoids long freeze every time). Not very important as we're unlikely to do anything else than one big load-all.
@@ -289,6 +410,8 @@ void sequence_directory_modal()
 	{
 		static widget::DirectoryInputText sequences_path_widget;
 		static std::vector<SequenceTemporaryData> sequences_in_dir;
+		static std::atomic<std::shared_ptr<std::unordered_map<size_t, std::vector<FigureImages>>>> loaded_sequences = nullptr;
+		static std::atomic<float> loaded_progress = 0.0f;
 
 		if (erase_successful_finished_threads())
 			rescan_files = true;
@@ -340,6 +463,7 @@ void sequence_directory_modal()
 					// Launch thread to export images with ArestiExporter
 					const fs::path file_path = sequences_path_widget.get_path() / (sequence_info.file_name + ".seq");
 					const fs::path textures_path = get_exported_textures_path(sequence_data.hash);
+					// Deref optional here, assume thread is never in map because button made it available
 					g_state.export_threads[sequence_data.hash].launch(file_path, textures_path)->detach();
 				}
 				// Disable text for rest of the row so it's grey, but we can still click the button
@@ -362,27 +486,14 @@ void sequence_directory_modal()
 
 		if (ImGui::Button("Save"))
 		{
-			// Somebody explain to me how THAT is better than a simple loop like below :(
-			// Rust felt better at doing this man
-			// // Sequences that were *maybe* loaded into VRAM, need to check optional!
-			// auto maybe_loaded_sequences = sequences_in_dir
-			// 	| std::views::transform([](auto& seq) { return seq.to_sequence_data(); })
-			// 	| std::ranges::to<std::vector>();
-			// // Filtered failed sequences to only keep ones that were actually loaded
-			// auto new_sequence_list = maybe_loaded_sequences
-			// 	| std::views::filter([](auto const& opt_seq) { return opt_seq.has_value(); })
-			// 	| std::views::transform([](auto&& opt_seq) { return std::move(*opt_seq); })
-			// 	| std::ranges::to<std::vector>();
-
-			std::vector<SequenceData> new_sequence_list;
-			for (SequenceTemporaryData const& temp_data : sequences_in_dir)
-			{
-				if (auto full_data = temp_data.to_sequence_data())
-					new_sequence_list.push_back(std::move(*full_data));
-			}
-			g_state.sequence_list = std::move(new_sequence_list);
-			g_state.sequences_dir = sequences_path_widget.get_path();
-			ImGui::CloseCurrentPopup();
+			// Loading images to RAM is very slow, spin up a thread to do it in parallel. It would be nice to load
+			// into VRAM in the thread too, but that's not possible due to Raylib + OpenGL limitations:
+			// https://www.reddit.com/r/raylib/comments/lxihj5/loadtexture_doesnt_work_in_a_separate_thread_c/
+			std::thread(&load_all_sequences_images_to_ram,
+				sequences_in_dir, std::ref(loaded_sequences), std::ref(loaded_progress)
+				).detach();
+			// Open popup which will stay on until loading is done
+			ImGui::OpenPopup("Loading sequences...");
 		}
 		ImGui::SameLine();
 		if (ImGui::Button("Cancel"))
@@ -394,17 +505,22 @@ void sequence_directory_modal()
 		ImGui::SameLine(); ImGui::SeparatorEx(ImGuiSeparatorFlags_Vertical); ImGui::SameLine();
 		if (ImGui::Button("Load all sequences"))
 		{
-			for (SequenceTemporaryData const& sequence : sequences_in_dir)
+			do_export_all_dotseq_files(sequences_path_widget.get_path(), sequences_in_dir);
+		}
+		// Progress bar modal, blocking all user interactions until all images have been loaded into the program
+		if (ImGui::BeginPopupModal("Loading sequences...", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove))
+		{
+			ImGui::ProgressBar(loaded_progress.load(), {500, 0});
+			// Finished loading all sequences images to RAM (that's slow), now just move stuff to VRAM (that's fast)
+			if (loaded_sequences.load() != nullptr)
 			{
-				if (sequence.are_files_cached())
-					continue;
-				const fs::path file_path = sequences_path_widget.get_path() / (sequence.info.file_name + ".seq");
-				const fs::path textures_path = get_exported_textures_path(sequence.hash);
-				if (auto thread = g_state.export_threads[sequence.hash].launch(file_path, textures_path))
-				{
-					thread->detach();
-				}
+				g_state.sequence_list = load_all_sequence_textures_to_vram(sequences_in_dir, *loaded_sequences.load());
+				g_state.sequences_dir = sequences_path_widget.get_path();
+				loaded_sequences = nullptr;
+				// Only close (both) popups & give control back when we've finished loading everything
+				ImGui::ClosePopupToLevel(0, true);
 			}
+			ImGui::EndPopup();
 		}
 		ImGui::EndPopup();
 	}
